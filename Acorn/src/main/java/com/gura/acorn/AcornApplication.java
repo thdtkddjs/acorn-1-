829a2c5ed9f48e2011494d94feb3476c56670f4c
seodaekyu <grarlove7@naver.com>
2023-01-13 15:14:21 +0900

    회원가입 폼 이메일 인증 관련 기능 구현중.. 추후 구현하겠음

--- Acorn/src/main/java/com/gura/acorn/AcornApplication.java
+++ Acorn/src/main/java/com/gura/acorn/AcornApplication.java
@@ -1,13 +1,29 @@
+
 package com.gura.acorn;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+//import com.gura.acorn.users.service.UsersEmailSenderService;
 
 @SpringBootApplication
 public class AcornApplication {
-
+	//@Autowired
+	//private UsersEmailSenderService senderService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AcornApplication.class, args);
 	}
-
+	
+	/* 회원가입할때 이메일인증을 거치는 코드 추후 구현하겠음
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail() {
+		senderService.sendEmail("xxx@gmail.com",
+								"This is Subject",
+								"This is Body of Email");
+	}
+	*/
 }
