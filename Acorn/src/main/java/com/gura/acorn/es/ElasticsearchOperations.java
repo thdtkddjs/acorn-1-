a553a5d5c917892a51812d07ce0c2ca0993f4cd0
songsangeon <thdtkddjs3@naver.com>
2023-02-17 16:23:44 +0900

    ES connection&service Ongoing

--- Acorn/src/main/java/com/gura/acorn/es/ElasticsearchOperations.java
+++
@@ -1,8 +0,0 @@
-package com.gura.acorn.es;
-
-import org.springframework.data.elasticsearch.core.DocumentOperations;
-import org.springframework.data.elasticsearch.core.SearchOperations;
-
-public interface ElasticsearchOperations extends DocumentOperations, SearchOperations {
-    //...
-}
